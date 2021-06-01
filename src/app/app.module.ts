87a2b3d431875ced341a90cbbd944aa35467aaf9
Rajeev Pai <rajeevpai.code@gmail.com>
2021-06-01 16:17:55 +0530

    Add basic routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule, Route } from '@angular/router';
 
 import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
@@ -9,6 +10,17 @@ import { MatButtonModule } from '@angular/material/button';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './auth/login/login.component';
 import { SignUpComponent } from './auth/sign-up/sign-up.component';
+
+const routes: Route[] = [
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'sign-up',
+    component: SignUpComponent
+  }
+];
 
 @NgModule({
   declarations: [
@@ -22,6 +34,7 @@ import { SignUpComponent } from './auth/sign-up/sign-up.component';
     MatCardModule,
     MatInputModule,
     MatButtonModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent]
