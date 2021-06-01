9877a99d68afd2a2cc3f77960e1f023af2e74a49
Rajeev Pai <rajeevpai.code@gmail.com>
2021-06-01 15:12:27 +0530

    Create login and sign-up components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { SignUpComponent } from './auth/sign-up/sign-up.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignUpComponent,
   ],
   imports: [
     BrowserModule
