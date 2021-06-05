e04272db7653e7e6a2b70ad0225a6c54a7226f44
Rajeev Pai <rajeevpai.code@gmail.com>
2021-06-05 10:14:31 +0530

    Create proxy config file

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -9,6 +9,6 @@ export class AuthService {
   constructor(private http: HttpClient) { }
 
   login() {
-    return this.http.get('https://jsonplaceholder.typicode.com/posts');
+    return this.http.get('http://localhost:3000/api/accounts/login');
   }
 }
