2473e6710dac2a7bb980134a870330150f69dde2
Rajeev Pai <rajeevpai.code@gmail.com>
2021-06-09 14:32:51 +0530

    Create contacts template

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,14 +2,15 @@ import { NgModule } from '@angular/core';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
+import { MatTableModule } from '@angular/material/table';
 
-import { ContactsComponent } from './contacts/contacts.component';
+import { ContactsComponent } from './pages/contacts/contacts.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { ContactComponent } from './contacts/contact/contact.component';
-import { ContactFormComponent } from './contacts/contact-form/contact-form.component';
-import { TransactionsComponent } from './transactions/transactions.component';
-import { TransactionComponent } from './transactions/transaction/transaction.component';
-import { TransactionFormComponent } from './transactions/transaction-form/transaction-form.component';
+import { ContactComponent } from './pages/contacts/contact/contact.component';
+import { ContactFormComponent } from './pages/contacts/contact-form/contact-form.component';
+import { TransactionsComponent } from './pages/transactions/transactions.component';
+import { TransactionComponent } from './pages/transactions/transaction/transaction.component';
+import { TransactionFormComponent } from './pages/transactions/transaction-form/transaction-form.component';
 
 @NgModule({
   declarations: [
@@ -24,6 +25,7 @@ import { TransactionFormComponent } from './transactions/transaction-form/transa
   imports: [
     MatToolbarModule,
     MatIconModule,
+    MatTableModule,
   ]
 })
 export class CoreModule { }
--- src/app/core/pages/contacts/contacts.component.ts
+++ src/app/core/pages/contacts/contacts.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
+  selector: 'mm-contacts',
   templateUrl: './contacts.component.html',
   styleUrls: ['./contacts.component.scss']
 })
