97b336d6fdbc63b909d9c4788cb603100ac7476f
Rajeev Pai <rajeevpai.code@gmail.com>
2021-06-20 14:31:09 +0530

    Update transaction view

--- src/app/core/pages/transactions/transactions.component.ts
+++ src/app/core/pages/transactions/transactions.component.ts
@@ -1,8 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { TransactionsService } from './transactions.service';
+import { Transaction } from '../../../helpers/types';
 
 @Component({
   selector: 'mm-transactions',
   templateUrl: './transactions.component.html',
   styleUrls: ['./transactions.component.scss']
 })
-export class TransactionsComponent { }
+export class TransactionsComponent implements OnInit {
+  transactions: Transaction[] = [];
+
+  constructor(
+    private transactionService: TransactionsService,
+  ) { }
+
+  ngOnInit() {
+    this.fetchTransactions();
+  }
+
+  fetchTransactions() {
+    this.transactionService
+      .fetchTransactionsOfAccount()
+      .subscribe(res => {
+        this.transactions = res.transactions;
+      })
+  }
+
+  refreshList() {
+
+  }
+}
