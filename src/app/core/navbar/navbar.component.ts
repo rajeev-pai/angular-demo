3edf2ca67794f5d6921ba443d70c214739d9e3f8
Rajeev Pai <rajeevpai.code@gmail.com>
2021-06-21 13:23:35 +0530

    Update navigation links

--- src/app/core/navbar/navbar.component.ts
+++ src/app/core/navbar/navbar.component.ts
@@ -1,8 +1,25 @@
 import { Component } from '@angular/core';
 
+interface NavLink {
+  name: string;
+  path: string;
+}
+
 @Component({
   selector: 'mm-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent { }
+export class NavbarComponent {
+
+  links: NavLink[] = [
+    {
+      name: 'Contacts',
+      path: '/contacts'
+    },
+    {
+      name: 'Transactions',
+      path: '/transactions'
+    }
+  ];
+}
