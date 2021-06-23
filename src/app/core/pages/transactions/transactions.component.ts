ae90732feec72fa8ce0b04a35135cc0529ed3164
Rajeev Pai <rajeevpai.code@gmail.com>
2021-06-23 06:53:52 +0530

    Update transactions page with user details

--- src/app/core/pages/transactions/transactions.component.ts
+++ src/app/core/pages/transactions/transactions.component.ts
@@ -1,22 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 import { TransactionsService } from './transactions.service';
 import { Transaction } from '../../../helpers/types';
+import { AuthService } from '../../../auth/auth.service';
 
 @Component({
-  selector: 'mm-transactions',
   templateUrl: './transactions.component.html',
   styleUrls: ['./transactions.component.scss']
 })
-export class TransactionsComponent implements OnInit {
+export class TransactionsComponent implements OnInit, OnDestroy {
   transactions: Transaction[] = [];
+  username = '';
+  email = '';
+
+  private subscription!: Subscription;
 
   constructor(
     private transactionService: TransactionsService,
+    private authService: AuthService,
   ) { }
 
   ngOnInit() {
+    this.subscription = this.authService.user$
+      .subscribe(user => {
+        if (user) {
+          this.username = user.username;
+          this.email = user.email;
+        }
+      });
+
     this.fetchTransactions();
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
   fetchTransactions() {
